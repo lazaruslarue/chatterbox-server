ffb097cf948d9e3cc0f725e7b8da7ec827ecac92
Marcus Phillips <github.com@marcusphillips.com>
2013-01-30 00:33:07 -0800

    new friending feature

--- js/utils.js
+++ js/utils.js
@@ -7,4 +7,4 @@
 var headerSetter = function(xhr) {
   xhr.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
   xhr.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
-}
+};

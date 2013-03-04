bb0af7ffc875dde4765415286cd311bbcbfefdc4
Marcus Phillips <github.com@marcusphillips.com>
2013-03-04 09:16:44 -0800

    not overwriting url params

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,8 @@
 if(!/(&|\?)username=/.test(window.location.search)){
-  var newSearch = window.location.search === '' || window.location.search === '?' ? '' : '&';
+  var newSearch = window.location.search;
+  if(newSearch !== '' & newSearch !== '?'){
+    newSearch += '&';
+  }
   newSearch += 'username=' + (prompt('What is your name?') || 'anonymous');
   window.location.search = newSearch;
 }

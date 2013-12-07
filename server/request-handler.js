140f4135e97c5c3a3e650472fdb1329be294ad00
Hack Reactor Students <github@hackreactor.com>
2013-12-06 20:44:08 -0800

    working!!!!one

--- server/request-handler.js
+++ server/request-handler.js
@@ -4,44 +4,44 @@
  * You'll have to figure out a way to export this function from
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
-
-var handleRequest = function(request, response) {
-  /* the 'request' argument comes from nodes http module. It includes info about the
-  request - such as what URL the browser is requesting. */
-
-  /* Documentation for both request and response can be found at
-   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
-
-  console.log("Serving request type " + request.method + " for url " + request.url);
-
-  var statusCode = 200;
-
-  /* Without this line, this server wouldn't work. See the note
-   * below about CORS. */
-  var headers = defaultCorsHeaders;
-
-  headers['Content-Type'] = "text/plain";
-
-  /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
-
-  /* Make sure to always call response.end() - Node will not send
-   * anything back to the client until you do. The string you pass to
-   * response.end() will be the body of the response - i.e. what shows
-   * up in the browser.*/
-  response.end("Hello, World!");
-};
-
-/* These headers will allow Cross-Origin Resource Sharing (CORS).
+var messageLog = [];
+var defaultCorsHeaders = {
+  /* These headers will allow Cross-Origin Resource Sharing (CORS).
  * This CRUCIAL code allows this server to talk to websites that
  * are on different domains. (Your chat client is running from a url
  * like file://your/chat/client/index.html, which is considered a
  * different domain.) */
-var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
+};
+
+var handleRequest = function(request, response) {
+  console.log("Serving request type " + request.method + " for url " + request.url);
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = "text/plain";
+  switch(request.method) {
+    case "GET":
+      if (request.url !== "/classes/messages" && (request.url !== "/classes/room1")) {
+        response.writeHead(404, headers); //ERROR NOT FOUND
+        response.end();
+      } else {
+        response.writeHead(200, headers); //okay
+        var responseMessage = JSON.stringify(messageLog);
+        response.end(responseMessage);
+      }
+      break;
+    case "POST":
+      var body = '';
+      response.writeHead(201, headers); //created
+      request.on('data', function( data) {
+        body += data;
+        messageLog.push(JSON.parse(body));
+      });
+      response.end();
+      break;
+  }
 };
 
 exports.handleRequest = handleRequest;
